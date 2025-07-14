eb753428e73a652d28e3df6fa1d98726bf7acd0f
Allan <allanbilfaqih214@gmail.com>
2025-07-14 13:37:08 +0700

    added foobar

---
+++ foobar/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Foobar
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int maxLimit = 15;
+            for (int i = 0; i < maxLimit; i++)
+            {
+                if (i % 3 == 0) Console.WriteLine("foo");
+                else if (i % 5 == 0) Console.WriteLine("bar");
+                else Console.WriteLine(i);
+            }
+        }
+    }
+}
