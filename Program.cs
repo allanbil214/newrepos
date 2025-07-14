59a404e1cb6b76234e05c74af92b85d72d312042
Allan <allanbilfaqih214@gmail.com>
2025-07-14 10:08:49 +0700

    update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace newrepos
+{
+    class newrepos
+    {
+        static void newrepos(string[] args)
+        {
+            Console.WriteLine("hello");
+        }
+    }
+}
