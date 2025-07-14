6dc86b9f2e8c490072c0b1ee1c6b96d078b884e9
Allan <allanbilfaqih214@gmail.com>
2025-07-14 11:59:36 +0700

    update 2

---
+++ helloworld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NewRepos
+{
+    class Calc
+    {
+        int input1;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+            input1 = Console.Write("Input 1: ");
+            Console.ReadLine();
+
+            Console.WriteLine(input1);
+
+        }
+    }
+}
