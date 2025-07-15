0147a2fc7727739d9648b7c45b454d6745f42234
Allan <allanbilfaqih214@gmail.com>
2025-07-15 11:49:34 +0700

    modified guessthenumber

--- guessthenumber/Program.cs
+++ guessthenumber/Program.cs
@@ -9,6 +9,7 @@ namespace GuessTheNumber
         public int howManyNumber = 0;
         public int thresholdHint = 0;
         public bool tryAgain = true;
+        
         public static void Main(string[] args)
         {
             Game game = new Game();
@@ -19,9 +20,11 @@ namespace GuessTheNumber
         {
             do
             {
-                Console.WriteLine("\n==== Welcome to Number Guesser ====\n");
-                InputHowManyNumber();
-                askHowHintThreshold();
+                Console.WriteLine("\n==== Welcome to Number Guesser ====");
+
+                howManyNumber = TryParsing("\n[=] How many number you want to guess?");
+                generatedNumber = GenerateTheNumber(howManyNumber);
+                thresholdHint = TryParsing("\n[=] How many threshold you want?");
 
                 Console.WriteLine($"\n[i] Now guess the number between 1 and {howManyNumber}");
                 LetTheUserGuess();
@@ -37,54 +40,15 @@ namespace GuessTheNumber
             return rand.Next(1, totalNumber);
         }
 
-        void InputHowManyNumber()
-        {
-            while (true)
-            {
-                Console.WriteLine("[=] How many number you want to guess?");
-                if (int.TryParse(Console.ReadLine(), out howManyNumber))
-                {
-                    generatedNumber = GenerateTheNumber(howManyNumber);
-                    // Console.WriteLine(generatedNumber); // debug
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("[!] Womp Womp, unrecognized input :()");
-                }
-            }
-        }
-
-        void askHowHintThreshold()
-        {
-            while (true)
-            {
-                Console.WriteLine("\n[=] How many threshold you want?");
-                if (int.TryParse(Console.ReadLine(), out thresholdHint))
-                {
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("[!] Womp Womp, unrecognized input :()");
-                }
-            }
-        }
         void LetTheUserGuess()
         {
             bool winning = false;
+
             while (winning == false)
             {
-                Console.WriteLine("\n[=] Can you guess the number?");
-                if (int.TryParse(Console.ReadLine(), out userGuesses))
-                {
-                    winning = GuessingNow(userGuesses, generatedNumber);
-                }
-                else
-                {
-                    Console.WriteLine("[!] Womp Womp, unrecognized input :()");
-                }
-            }
+                userGuesses = TryParsing("\n[=] Can you guess the number?");
+                winning = GuessingNow(userGuesses, generatedNumber);
+            } 
         }
 
         bool GuessingNow(int guessedNumber, int numberGenerated)
@@ -92,7 +56,7 @@ namespace GuessTheNumber
             if (guessedNumber == numberGenerated)
             {
                 Console.WriteLine("\n[i] Congrats you guessess correctly, you win!!\n");
-                Console.WriteLine("[=] Do you want to replay the game? (y/n)");
+                Console.WriteLine("[=] Do you want to replay the game? (Insert 'y' to replay)");
                 if (Console.ReadLine() == "y")
                 {
                     tryAgain = true;
@@ -105,7 +69,6 @@ namespace GuessTheNumber
             {
                 Console.WriteLine("[i] Sorry but woah, you are closer than you thought!");
             }
-
             else if (guessedNumber < numberGenerated)
             {
                 Console.WriteLine("[i] Sorry you are wayy lower than you thought, try higher number");
@@ -120,6 +83,22 @@ namespace GuessTheNumber
             }
             // Console.WriteLine($"{guessedNumber} | {numberGenerated}"); // debug
             return false;
+        }
+
+        int TryParsing(string msg)
+        {
+            while (true)
+            {
+                Console.WriteLine(msg);
+                if (int.TryParse(Console.ReadLine(), out int value))
+                {
+                    return value;
+                }
+                else
+                {
+                    Console.WriteLine("[!] Womp Womp, unrecognized input :()");
+                }
+            }
         }
     }
 }
