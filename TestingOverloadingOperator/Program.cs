fccfe2ff81a91f87eb66c15406ffa05f4336b5e8
Allan <allanbilfaqih214@gmail.com>
2025-07-21 10:07:52 +0700

    added TestingOverloadingOperator

---
+++ TestingOverloadingOperator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TestingOverloadingOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+            Anu xAnu = new Anu { value = 1 };
+            Anu xAnu2 = new Anu { value = 2 };
+
+            Anu yAnu = xAnu + xAnu2;
+
+            Console.WriteLine(yAnu.value);
+        }
+
+
+    }
+
+    public class Anu
+    {
+        public int value;
+
+        public static Anu operator +(Anu a, Anu b)
+        {
+            return new Anu { value = a.value + b.value };
+        }
+    }
+}
